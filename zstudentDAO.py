8c7db85a721c961fd4919d41618538bef5a9da7c
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2019-12-17 13:45:49 +0000

    Lab9

---
+++ zstudentDAO.py
@@ -0,0 +1,69 @@
+import mysql.connector
+class StudentDAO:
+    db=""
+    def __init__(self):
+        self.db = mysql.connector.connect(
+        host="localhost",
+        port="3306",
+        user="root",
+        password="kim123",
+    #user="gerardh",
+    #password="Nov2019123"
+        database="datarepresentation"
+        )
+    def create(self, values):
+        cursor = self.db.cursor()
+        sql="insert into student (name, age) values (%s, %s)"
+
+        cursor.execute(sql, values)
+        self.db.commit()
+        return cursor.lastrowid
+    
+    def getAll(self):
+        cursor = self.db.cursor()
+        sql="select * from student"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            returnArray.append(self.convertToDictionary(result))
+        return returnArray
+    
+    def findByID(self, id):
+        cursor = self.db.cursor()
+        sql = "select * from student where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return self.convertToDictionary(result)
+
+    def update(self, values):
+        cursor = self.db.cursor()
+        sql = "update student set name= %s, age=%s where id = %s"
+        cursor.execute(sql, values)
+        self.db.commit()
+    
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql="delete from student where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        print("delete done")
+
+    def convertToDictionary(self, result):
+        colnames=['id', 'name', 'age']
+
+        item = {}
+
+        for i, colName in enumerate(colnames):
+            value = result[i]
+            item[colName] = value
+            
+        return item
+
+StudentDAO = StudentDAO()
